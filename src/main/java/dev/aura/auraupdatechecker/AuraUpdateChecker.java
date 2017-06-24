1298d1129f6132aca906bd031e28096faefe9106
BrainStone <yannick@tedworld.de>
2017-06-25 01:25:27 +0200

    Added main class

---
+++ src/main/java/dev/aura/auraupdatechecker/AuraUpdateChecker.java
@@ -0,0 +1,13 @@
+package dev.aura.auraupdatechecker;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = AuraUpdateChecker.ID, name = AuraUpdateChecker.NAME, version = AuraUpdateChecker.VERSION, description = AuraUpdateChecker.DESCRIPTION, authors = {
+        AuraUpdateChecker.AUTHOR_BRAINSTONE })
+public class AuraUpdateChecker {
+    public static final String ID = "@id@";
+    public static final String NAME = "@name@";
+    public static final String VERSION = "@version@";
+    public static final String DESCRIPTION = "@description@";
+    public static final String AUTHOR_BRAINSTONE = "BrainStone";
+}
