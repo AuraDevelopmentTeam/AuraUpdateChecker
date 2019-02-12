2dcceb7b01494988ad290ac4b474cafd7a0e26b9
BrainStone <yannick@tedworld.de>
2019-02-12 23:49:34 +0100

    Added helper class to store plugin versions

---
+++ src/main/java/dev/aura/updatechecker/checker/PluginVersionInfo.java
@@ -0,0 +1,25 @@
+package dev.aura.updatechecker.checker;
+
+import com.google.common.collect.ImmutableMap;
+import dev.aura.lib.version.Version;
+import java.sql.Timestamp;
+import java.util.SortedMap;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
+@Value
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class PluginVersionInfo {
+  private final Version recommendedVersion;
+  @EqualsAndHashCode.Exclude private final Version latestVersion;
+  private final ImmutableMap<Timestamp, Version> allVersions;
+
+  public PluginVersionInfo(Version recommendedVersion, SortedMap<Timestamp, Version> allVersions) {
+    this(
+        recommendedVersion,
+        allVersions.get(allVersions.firstKey()),
+        ImmutableMap.copyOf(allVersions));
+  }
+}
