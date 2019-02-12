d4c26a7a9213310beb14465b7205575ed26907f1
BrainStone <yannick@tedworld.de>
2019-02-12 23:49:24 +0100

    Implemented getRecommendedVersion

--- src/main/java/dev/aura/updatechecker/checker/OreAPI.java
+++ src/main/java/dev/aura/updatechecker/checker/OreAPI.java
@@ -1,11 +1,17 @@
 package dev.aura.updatechecker.checker;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dev.aura.lib.version.Version;
 import dev.aura.updatechecker.AuraUpdateChecker;
 import dev.aura.updatechecker.util.PluginContainerUtil;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ProtocolException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.net.ssl.HttpsURLConnection;
 import lombok.Cleanup;
@@ -16,9 +22,11 @@ import org.spongepowered.api.plugin.PluginContainer;
 public class OreAPI {
   public static final String API_URL = "https://ore.spongepowered.org/api/v1/";
   public static final String PROJECT_CALL = "projects/<pluginId>";
+  public static final String VERSION_CALL = PROJECT_CALL + "/versions";
   public static final int DEFAULT_TIMEOUT = 250;
 
-  private static AtomicInteger errorCounter = new AtomicInteger(0);
+  private static final AtomicInteger errorCounter = new AtomicInteger(0);
+  private static final Gson gson = new Gson();
 
   public static int getErrorCounter() {
     return errorCounter.get();
@@ -33,6 +41,7 @@ public class OreAPI {
       @Cleanup("disconnect")
       HttpsURLConnection connection = getConnectionForCall(PROJECT_CALL, plugin);
       applyDefaultSettings(connection);
+      connection.setRequestMethod("HEAD"); // We don't care about the content yet
       connection.connect();
 
       return connection.getResponseCode() == 200;
@@ -53,6 +62,50 @@ public class OreAPI {
       }
 
       return false;
+    }
+  }
+
+  public static Optional<Version> getRecommendedVersion(PluginContainer plugin) {
+    try {
+      @Cleanup("disconnect")
+      HttpsURLConnection connection = getConnectionForCall(PROJECT_CALL, plugin);
+      applyDefaultSettings(connection);
+      connection.connect();
+
+      final String recommendedVersion =
+          gson.fromJson(
+                  new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8),
+                  JsonObject.class)
+              .get("recommended")
+              .getAsJsonObject()
+              .get("name")
+              .getAsString();
+
+      AuraUpdateChecker.getLogger()
+          .debug(
+              "Recommended Version for plugin "
+                  + PluginContainerUtil.getPluginString(plugin)
+                  + " is: "
+                  + recommendedVersion);
+
+      return Optional.of(new Version(recommendedVersion));
+    } catch (ClassCastException | IOException | URISyntaxException | IllegalStateException e) {
+      if (errorCounter.incrementAndGet() == 1) {
+        AuraUpdateChecker.getLogger()
+            .warn(
+                "Could not contact the Ore Repository API for plugin "
+                    + PluginContainerUtil.getPluginString(plugin),
+                e);
+      } else {
+        AuraUpdateChecker.getLogger()
+            .warn(
+                "Could not contact the Ore Repository API for plugin "
+                    + PluginContainerUtil.getPluginString(plugin)
+                    + ": "
+                    + e.getClass().getName());
+      }
+
+      return Optional.empty();
     }
   }
 
