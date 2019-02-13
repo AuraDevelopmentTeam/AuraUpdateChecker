bf778773651e565ded4ced1cacc768f8b92d682a
BrainStone <yannick@tedworld.de>
2019-02-13 21:38:55 +0100

    Allow any type of Map in PluginVersionInfo
    - If it's the same comparator (which it is by default) the copying is super fast!

--- src/main/java/dev/aura/updatechecker/util/PluginVersionInfo.java
+++ src/main/java/dev/aura/updatechecker/util/PluginVersionInfo.java
@@ -2,24 +2,26 @@ package dev.aura.updatechecker.util;
 
 import com.google.common.collect.ImmutableMap;
 import dev.aura.lib.version.Version;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.Map;
 import java.util.SortedMap;
-import lombok.AccessLevel;
+import java.util.TreeMap;
 import lombok.EqualsAndHashCode;
-import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 @Value
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class PluginVersionInfo {
   private final Version recommendedVersion;
   @EqualsAndHashCode.Exclude private final Version latestVersion;
   private final ImmutableMap<Date, Version> allVersions;
 
-  public PluginVersionInfo(Version recommendedVersion, SortedMap<Date, Version> allVersions) {
-    this(
-        recommendedVersion,
-        allVersions.get(allVersions.firstKey()),
-        ImmutableMap.copyOf(allVersions));
+  public PluginVersionInfo(Version recommendedVersion, Map<Date, Version> allVersions) {
+    final SortedMap<Date, Version> allVersionsSorted = new TreeMap<>(Comparator.reverseOrder());
+    allVersionsSorted.putAll(allVersions);
+
+    this.recommendedVersion = recommendedVersion;
+    this.latestVersion = allVersions.get(allVersionsSorted.firstKey());
+    this.allVersions = ImmutableMap.copyOf(allVersionsSorted);
   }
 }
