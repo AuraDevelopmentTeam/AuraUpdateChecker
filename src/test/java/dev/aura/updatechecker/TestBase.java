e91e5c5575965f1ed8ea461e6ad52d7eb13d1c7f
BrainStone <yannick@tedworld.de>
2019-02-24 13:04:13 +0100

    Allow messages to be translated during tests

--- src/test/java/dev/aura/updatechecker/TestBase.java
+++ src/test/java/dev/aura/updatechecker/TestBase.java
@@ -1,5 +1,7 @@
 package dev.aura.updatechecker;
 
+import dev.aura.lib.messagestranslator.MessagesTranslator;
+import dev.aura.lib.messagestranslator.unittesthelper.UnitTestMessagesTranslator;
 import dev.aura.updatechecker.config.Config;
 import org.junit.BeforeClass;
 import org.slf4j.Logger;
@@ -8,6 +10,8 @@ import org.slf4j.LoggerFactory;
 public class TestBase {
   protected static final Logger DEFAULT_LOGGER = LoggerFactory.getLogger(AuraUpdateChecker.NAME);
   protected static final Config DEFAULT_CONFIG = new Config();
+  protected static final MessagesTranslator DEFAULT_TRANSLATOR =
+      new UnitTestMessagesTranslator(AuraUpdateChecker.ID);
 
   @BeforeClass
   public static void setupPlugin() {
@@ -16,6 +20,7 @@ public class TestBase {
 
       instance.logger = DEFAULT_LOGGER;
       instance.config = DEFAULT_CONFIG;
+      instance.translator = DEFAULT_TRANSLATOR;
     }
   }
 }
