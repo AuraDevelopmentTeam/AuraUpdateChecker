14e34635ddc42796fa742a1c078f13b3cbe06376
BrainStone <yannick@tedworld.de>
2019-02-28 12:56:53 +0100

    Added additional checks and safeteys

--- src/main/java/dev/aura/updatechecker/event/PlayerEvents.java
+++ src/main/java/dev/aura/updatechecker/event/PlayerEvents.java
@@ -6,11 +6,12 @@ import java.util.concurrent.TimeUnit;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.service.pagination.PaginationList;
 
 public class PlayerEvents {
-  @Listener
+  @Listener(order = Order.POST)
   public void onPlayerJoin(ClientConnectionEvent.Join event) {
     final Player player = event.getTargetEntity();
 
@@ -28,7 +29,8 @@ public class PlayerEvents {
 
                 // Check again before actually executing, just to be sure the player can still
                 // receive the message
-                if ((pagination != null)
+                if (player.isOnline()
+                    && (pagination != null)
                     && player.hasPermission(PermissionRegistry.NOTIFICATION_UPDATE_AVAIABLE_JOIN)) {
                   pagination.sendTo(player);
                 }
